1a4664fc5ad09779fe02a6545456acab13022b7b
Ifrim Alexandra <aleifrim79@gmail.com>
2025-11-03 22:16:42 +0200

    implemented dynamic programing method

--- Assignment_45/dynamic_1.py
+++ Assignment_45/dynamic_1.py
@@ -1,26 +1,77 @@
-def backtracking(n, m):
-    if n < m:
-        return
-    seq = []
-    used = [False] * (n+1)
-    found = [False]
-    def generate():
-        if len(seq) == n:
-            print(seq)
-            found[0] = True
-            return
-        for i in range(1, n+1):
-            if not used[i] and (not seq or abs(seq[-1] - i)):
-                seq.append(i)
-                used[i] = True
-                generate()
+def menu():
+    while True:
+        print("\nChoose an option from the following list: ")
+        print(" 1. Naive Version \n 2. Dynamic Version \n 3. Exit \n")
+        try:
+            o=int(input("Type your option: "))
+        except ValueError:
+            print("Invalid input. You must choose one of the options 1-3.")
+            continue
+        if o == 1:
+            n = input("Enter the first list: ")
+            m = input("Enter the second list: ")
+            length, seq = lcs_naive(n, m)
+            print("The maximum length is:", length)
+            print("This is a valid sequence:", seq)
+        elif o == 2:
+            n = input("Enter the first list: ")
+            m = input("Enter the second list: ")
+            length, seq = lcs_dynamic(n, m)
+            print("The maximum length is:", length)
+            print("This is a valid sequence:", seq)
+        elif o == 3:
+            print("Thanks for playing!")
+            exit()
+        else:
+            print("You must choose one of the options 1-3.")
+
+def lcs_dynamic(x, y):
+    #O(m*n)
+    #we calculate each cell
+    m, n = len(x)+1, len(y)+1
+    res = [[0 for _ in range(n)] for _ in range(m)] #completed first row and column with 0
+    for i in range(1, m):
+        for j in range(1, n):
+            if x[i-1] == y[j-1]:
+                res[i][j] = res[i-1][j-1] + 1
+            else:
+                res[i][j] = max(res[i][j-1], res[i-1][j])
+
+    length = res[m-1][n-1]
+    i, j = m-1, n-1
+    subseq = []
+    while i > 0 and j > 0:
+        if x[i - 1] == y[j - 1]:
+            subseq.append(x[i - 1])
+            i -= 1
+            j -= 1
+        elif res[i - 1][j] > res[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+    subseq.reverse()
+    return length, ''.join(subseq)
+
+def lcs_naive(x, y):
+    #O(2^(m+n))
+    #for each number are 2 options: keep it or ignore it
+    #so 2^n * 2^m
+    if not x or not y:
+        return 0, ""
+    if x[-1] == y[-1]:
+        length, subseq = lcs_naive(x[:-1], y[:-1])
+        return length + 1, subseq + x[-1]
+    else:
+        l1, s1 = lcs_naive(x[:-1], y)
+        l2, s2 = lcs_naive(x, y[:-1])
+        if l1 > l2:
+            return l1, s1
+        else:
+            return l2, s2
 
 
 def main():
-    n = int(input("Enter the maximum number in the list: "))
-    m = int(input("Enter the minimum difference between two consecutive numbers: "))
-    print("These are the valid lists:")
-    backtracking(n, m)
+    menu()
 
 if __name__ == "__main__":
     main()
